62658eb3e0193c40d5e801316afd4deeb107fe4e
ndalton12 <niall.dalton12@gmail.com>
2020-11-10 18:35:44 -0500

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Alpha Proxy
+# AMP
 By Team DominateTheStreetsYeetTheSkeet
 
--- src/main.py
+++ src/main.py
@@ -2,4 +2,3 @@ from ray import tune
 from ray.rllib.agents.ppo import PPOTrainer
 
 tune.run(PPOTrainer, config={"env": "CartPole-v0", "framework": "torch"})
-
