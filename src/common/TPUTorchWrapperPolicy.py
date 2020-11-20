452b643f74a5c6e7aeb1bea160076ada85d09e26
ndalton12 <niall.dalton12@gmail.com>
2020-11-20 00:51:22 -0500

    asdf

--- src/common/TPUTorchWrapperPolicy.py
+++ src/common/TPUTorchWrapperPolicy.py
@@ -85,8 +85,8 @@ class TPUTorchWrapperPolicy(TorchPolicy):
         Policy.__init__(self, observation_space, action_space, config)
 
         counter = ray.get_actor("global_counter")
-        counter.inc.remote(1)
-        count = ray.get(counter.get.remote())
+        await counter.inc.remote(1)
+        count = ray.get(counter.get.remote(0))
         print(f"{count}********************")
         #print(xm.get_xla_supported_devices())
         self.device = xm.xla_device(n=count)  # DIFFERENCE HERE FOR TPU USAGE
